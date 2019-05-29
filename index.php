fa07cab5bef6bf9dcaa0299a455a43170da89066
Raziel Miranda <raziel.miranda@galata.com.br>
2019-05-29 00:56:11 -0300

    echo vs print

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php 
 //My php intro
 
+$text = "Outputs words";
+$y = 2;
+$x = 22;
 
+print "<h1>echo vs print</h1>";
 
+echo $x / $y;
+echo ($x / $y);
+print $x / $y;
+print ($x / $y);
 
-
-
+print("<p>more or less different".$text);
 
 ?>
