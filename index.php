0faa7ba87ff52de20f2757efb3c7844fc39c751a
Raziel Miranda <raziel.miranda@galata.com.br>
2019-06-01 20:32:53 -0300

    my sql

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
-<?php include 'header.php';  ?>
+<?php
+	$mysqli = new mysqli('127.0.0.1', 'root', 'database@LOCAL', '');
+
+	if ($mysqli->connect_error) {
+	    die('Connect Error (' . $mysqli->connect_errno . ') '
+	            . $mysqli->connect_error);
+	}
+
+include 'header.php';  
+?>
+
   
-  <body>
+<body>
       <?php require 'menu.php'?>
+</body>
 
-        <?php
-            $var = 3 * 3;
-            echo $var;
-        ?>
-  </body>
 
-<?php include 'footer.php'; ?>
+<?php 
+	include 'footer.php'; 
+	$mysqli->close();
+?>
