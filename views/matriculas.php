7c249b16b1f37aae5b9ccd95048d1e774cc02f1b
Raziel Miranda <raziel.miranda@galata.com.br>
2019-07-13 15:22:31 -0300

    Update matriculas.php

--- views/matriculas.php
+++ views/matriculas.php
@@ -1,1 +1,23 @@
-Matriculas
+<table>
+	<tr>
+		<th>
+			<td>ALUNO MATRICULADO</td>
+		</th>
+	</tr>
+
+	<tr>
+		<th>
+			<td>CURSO MATRICULADO</td>
+		</th>
+	</tr>
+
+	<?php 
+
+	while ($linha = mysqli_fetch_array($consulta_matriculas)) {
+		echo "<tr><td>".$linha['ID_ALUNOS'].'</td>';
+		echo "<td>".$linha['ID_CURSOS'].'</td></tr>';
+	}
+
+	 ?>
+
+</table>
