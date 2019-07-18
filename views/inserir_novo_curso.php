0ae0a272bfde848549edd5921dc44b41701d0c8e
Raziel Miranda <raziel.miranda@galata.com.br>
2019-07-18 02:15:53 -0300

    UPDATE FUNCIONANDO

--- views/inserir_novo_curso.php
+++ views/inserir_novo_curso.php
@@ -12,19 +12,20 @@
 
 <?php }else{ ?>
 	<?php while ($linha = mysqli_fetch_array($consulta_cursos)) { ?>
-
+		<?php if($linha['ID_CURSOS'] == $_GET['editar']){ ?>
 
 <h1>Editar curso</h1>
 <form method="post" action="edita_curso.php">
 	<br>
-	<input type="text" name="nome_curso" placeholder="Editar nome curso" value="<?php echo $linha['NOME_CURSOS']; ?>">
+	<input type="hidden" name="ID_CURSOS" value="<?php echo $linha['ID_CURSOS']; ?>">
+	<input type="text" name="NOME_CURSOS" placeholder="Editar nome curso" value="<?php echo $linha['NOME_CURSOS']; ?>">
 	<br>
-	<input type="text" name="carga_horaria" placeholder="Editar carga horaria" value="<?php echo $linha['CARGA_HORARIA']; ?>">
+	<input type="text" name="CARGA_HORARIA" placeholder="Editar carga horaria" value="<?php echo $linha['CARGA_HORARIA']; ?>">
 	<br>
 	<input type="submit" value="Editar curso" >
 </form>
 
 
-
+		<?php } ?>
 	<?php } ?>
 <?php } ?>
