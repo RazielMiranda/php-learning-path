c3ac079f812a20ce9b07cfbd9294eddc49d2512b
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2020-03-26 23:34:05 -0300

    iniciando datetime

--- index.php
+++ index.php
@@ -1,37 +1,20 @@
-<?php include 'ContaBancaria.class.php'; ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>BANCO PHP</title>
+	<title>DATE TIME</title>
 </head>
 <body>
-<main>
-<h1>Bem vindo ao banco PHP!</h1>
-<hr>
-<br>
 <?php 
 
-$conta = new ContaBancaria(
-	'Nubank',
-	'Raziel M.',
-	'9832-8',
-	'0001',
-	0
-);
 
 
-echo $conta->obterSaldo();
 
-echo $conta->depositar(300.00);
 
-echo $conta->obterSaldo();
 
-echo $conta->saque(150.00);
 
-echo $conta->obterSaldo();
+
 
 
 ?>
-</main>
 </body>
 </html>
